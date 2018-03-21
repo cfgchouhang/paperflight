ab41d66669f1e4ebce99eebbfca0268b3afd5edc
Pnerd <cfgchouhang@gmail.com>
2018-03-21 11:16:54 +0800

    fix camera zoom-out issue

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour {
     private Vector3 offset, newPosition;
-    private float yBound, elapsedTime, x, z, yPrev;
+    private float yBound, elapsedTime, z, yPrev;
     public GameObject player, background;
     public Text text;
     public float zMin, zMax;
@@ -19,21 +19,23 @@ public class CameraController : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        x = player.transform.position.x + offset.x;
+        //x = player.transform.position.x + offset.x;
         z = transform.position.z;
         float py = player.transform.position.y;
 
         elapsedTime += Time.deltaTime;
         if (elapsedTime > 0.25f) {
-            text.text = (py / yBound).ToString();
+            text.text = (py / yBound).ToString()+" "+yBound+" "+py;
         }
        
-        if(py / yBound > 0.1f && py > yPrev) {
+        if(py > 10f && py > yPrev) {
             if(z > zMin) {
                 z -= (zMax - zMin) * (1f+Mathf.Exp(-2f * py / yBound))  * 0.2f * Time.deltaTime;
             }
-        } else if(z < zMax) {
-            z += (zMax - zMin) * (1f+Mathf.Exp(-2f * py / yBound)) * 0.2f * Time.deltaTime;
+        } else if(py < 25f && py < yPrev) {
+            if(z < zMax) {
+                z += (zMax - zMin) * (1f + Mathf.Exp(-2f * py / yBound)) * 0.2f * Time.deltaTime;
+            }
         }
 
         transform.position = new Vector3(
