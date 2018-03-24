d5de81f0ff6cfcbce893efd4a3ec58fb0c5cbc5f
Pnerd <cfgchouhang@gmail.com>
2018-03-24 17:17:02 +0800

    fix camera zooming

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour {
     private Vector3 offset, newPosition;
-    private float yBound, elapsedTime, z, yPrev;
+    private float yBound, elapsedTime, z, x, yPrev;
     public GameObject player, background;
     public Text text;
     public float zMin, zMax;
@@ -15,11 +15,13 @@ public class CameraController : MonoBehaviour {
         offset = transform.position - player.transform.position;
         yBound = background.GetComponent<Collider>().bounds.size.y / 2f;
         yPrev = 0f;
+        x = 0f;
+
     }
 
     // Update is called once per frame
     void Update () {
-        //x = player.transform.position.x + offset.x;
+        x = 0f;
         z = transform.position.z;
         float py = player.transform.position.y;
 
@@ -31,15 +33,20 @@ public class CameraController : MonoBehaviour {
         if(py > 10f && py > yPrev) {
             if(z > zMin) {
                 z -= (zMax - zMin) * (1f+Mathf.Exp(-2f * py / yBound))  * 0.2f * Time.deltaTime;
+                x = 8f * (1f + Mathf.Exp(-2f * py / yBound)) * Time.deltaTime;
             }
         } else if(py < 25f && py < yPrev) {
             if(z < zMax) {
                 z += (zMax - zMin) * (1f + Mathf.Exp(-2f * py / yBound)) * 0.2f * Time.deltaTime;
+                x = -8f * (1f + Mathf.Exp(-2f * py / yBound)) * Time.deltaTime;
+
             }
         }
 
+        offset.x += x;
+
         transform.position = new Vector3(
-            player.transform.position.x + offset.x * z / offset.z,
+            player.transform.position.x  + offset.x,
             transform.position.y,
             z
         );
